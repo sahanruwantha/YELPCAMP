a188718c2b9b83a19568f0771ea3b594622cb301
sahanruwanthasilva <sahanr.silva@hotmail.com>
2022-05-03 09:36:27 +0530

    added authorization

--- routes/reviews.js
+++ routes/reviews.js
@@ -3,7 +3,7 @@ const router = express.Router({mergeParams:true});
 const cathAsync = require('../utils/catchAsync');
 const Campground = require('../models/campground')
 const Review = require('../models/review');
-const { validateReview, isLoggedIn } = require('../middleware');
+const { validateReview, isLoggedIn, isReviewAuthor } = require('../middleware');
 
 
 router.post('/', isLoggedIn ,validateReview ,cathAsync(async (req, res)=>{
@@ -16,7 +16,7 @@ router.post('/', isLoggedIn ,validateReview ,cathAsync(async (req, res)=>{
     res.redirect(`/campgrounds/${campgroud._id}`);
 }));
 
-router.delete('/:reviewId',isLoggedIn ,cathAsync( async (req, res)=>{
+router.delete('/:reviewId',isLoggedIn ,isReviewAuthor , cathAsync( async (req, res)=>{
     const { id, reviewId } = req.params;
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
