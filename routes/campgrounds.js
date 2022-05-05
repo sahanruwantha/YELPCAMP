e6674cf22741aa2e1ad48093d3f411e4c53a82ba
sahanruwanthasilva <sahanr.silva@hotmail.com>
2022-05-05 09:40:32 +0530

    added .env

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const cathAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgroud = require('../controllers/campground')
-
+const multer  = require('multer')
+const upload = multer({ dest: 'uploads/' })
 
 router.route('/')
     .get( cathAsync(campgroud.index))
     //.post(isLoggedIn ,validateCampground ,cathAsync(campgroud.createCampground));
-    
+    .post(upload.single('image'), (req,res)=>{
+        
+    })
+
     
 router.get('/new', isLoggedIn , campgroud.renderNewForm);
     
