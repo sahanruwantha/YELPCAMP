773f900244c99349ed4668479d2c694018b79a0a
sahanruwanthasilva <sahanr.silva@hotmail.com>
2022-05-06 07:27:35 +0530

    added cloudinary image upload feature

--- models/campground.js
+++ models/campground.js
@@ -4,7 +4,10 @@ const Schema = mongoose.Schema;
 
 const CampGroundSchema = new Schema({
     title: String,
-    image: String,
+    images: [{
+        url: String,
+        filename: String
+    }],
     price: Number, 
     description: String,
     location: String,
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -3,15 +3,18 @@ const router = express.Router();
 const cathAsync = require('../utils/catchAsync');
 const { isLoggedIn, isAuthor, validateCampground } = require('../middleware');
 const campgroud = require('../controllers/campground')
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+const multer  = require('multer');
+const { storage } = require( '../cloudinary' );
+const { redirect } = require( 'express/lib/response' );
+const upload = multer({ storage })
 
 router.route('/')
     .get( cathAsync(campgroud.index))
-    //.post(isLoggedIn ,validateCampground ,cathAsync(campgroud.createCampground));
-    .post(upload.single('image'), (req,res)=>{
-        
-    })
+    .post(isLoggedIn ,validateCampground , upload.array('image'),cathAsync(campgroud.createCampground));
+    // .post(upload.single('image'), (req,res)=>{
+    //     console.log(req.body, req.file)
+    //     res.send('it worked');
+    // })
 
     
 router.get('/new', isLoggedIn , campgroud.renderNewForm);
